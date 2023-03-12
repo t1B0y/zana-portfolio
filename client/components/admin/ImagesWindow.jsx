6a42f8aa3060e5e772683f7659697ddfba85695b
t1B0y <57271147+t1B0y@users.noreply.github.com>
2023-03-11 21:50:56 -0500

    fixed upload image window

--- client/components/admin/ImagesWindow.jsx
+++ client/components/admin/ImagesWindow.jsx
@@ -4,12 +4,12 @@ import { fetchimages } from '../../redux/projectsSlice.js';
 import { Image } from './styledComponents/AdminComponents';
 
 const ImagesWindow = (props) => {
-  const imageSelected = useSelector((state) => state.projects.currentImage);
-
+  const currentImage = useSelector((state) => state.projects.currentImage);
+  const project = useSelector((state) => state.projects.currentProject);
   return (
     <div>
       <h4>Change Image</h4>
-      <Image src={imageSelected} />
+      <Image src={project.files[currentImage]} />
     </div>
   );
 };
